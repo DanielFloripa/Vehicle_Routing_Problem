f5eb510650ba639ea56de0550e85a5bcc9827f89
Daniel <daniel@colmeia.udesc.br>
2019-08-27 23:02:25 -0300

    Control inside cargo object

--- cargo.py
+++ cargo.py
@@ -8,5 +8,6 @@ class Cargo(Models):
     def __init__(self, element):
         super().__init__(*element)
         self.can_be_carried_by = None
-        self.will_travel = 0
+        self.will_travel_shortest_distance = np.inf
+        self.potential_distances = []
 
